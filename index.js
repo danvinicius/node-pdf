e95777d24838f269b0c0b3772e1ea6ce0f9db54d
Daniel Vinícius <viniccius774@gmail.com>
2022-03-04 16:23:48 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const pdf = require("html-pdf");
+const ejs = require("ejs");
+
+// const data = {
+//   name: "Daniel",
+//   stack: "Back-end developer",
+// };
+
+ejs.renderFile(
+  "./myFile.ejs",
+  //   {
+  //     name: data.name,
+  //     stack: data.stack,
+  //   },
+  (err, html) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    pdf.create(html, {}).toFile("./result.pdf", (err, res) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    });
+  }
+);
